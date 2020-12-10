f58b51b0ae46f513f46bcaa81b908f620d0b8474
Thaekeh <thaekeh@gmail.com>
2020-12-10 17:09:48 +0100

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AudioPlayer msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AudioPlayer from './components/AudioPlayer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AudioPlayer
   }
 }
 </script>
